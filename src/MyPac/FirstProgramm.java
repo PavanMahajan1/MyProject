8c8ac7fac253c4c50507a28e9acbf8da2c502e80
Pavan Mahajan <mahajanpavan232@gmail.com>
2023-02-05 23:06:09 +0530

    First commit

---
+++ src/MyPac/FirstProgramm.java
@@ -0,0 +1,13 @@
+package MyPac;
+
+public class FirstProgramm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello to Eclipse");
+		System.out.println(args[0]+" "+args[1]);
+
+
+	}
+
+}
---
+++ src/com/java/basic/HelloCountry.java
@@ -0,0 +1,16 @@
+package com.java.basic;
+import java.util.*;
+
+public class HelloCountry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, India");
+		System.out.println(args[0]+args[1]);
+		int a = 10;
+		int b = 20-10;
+	//	System.out.println(a.isEqual(b)?"Same":"Diff");
+	}
+
+}
+
